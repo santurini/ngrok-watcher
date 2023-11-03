84660ae9087f05f1d0313a7226669c6970c4919a
Arturo Ghinassi <ghinassiarturo8@gmail.com>
2023-11-03 13:38:13 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,12 +9,21 @@ cd $SCRIPT_DIR
 
 URL_FILE=README.md
 
+if [ -e "$URL_FILE" ]; then
+    rm "$URL_FILE" && touch "$URL_FILE"
+else
+    touch "$URL_FILE"               
+fi
+
 while true ; do
     OLD_URL=$(cat "$URL_FILE")
     NGROK_URL="$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[].public_url' | sort )"
 
     if [ "$NGROK_URL" != "$OLD_URL" ] ; then
-        echo "# Current URL \n\`\`\`\n"$NGROK_URL"\n\`\`\`" > "$URL_FILE"
+        echo "# Current URL" >> "$URL_FILE"
+        echo "\`\`\`" >> "$URL_FILE"
+        echo "$NGROK_URL" >> "$URL_FILE"
+        echo "\`\`\`" >> "$URL_FILE"
         git add "$URL_FILE"
         git commit -m "new url - $(date)"
         git push
